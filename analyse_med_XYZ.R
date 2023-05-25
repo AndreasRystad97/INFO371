c3f6be5b6e50ba377cc93905ec00e94d5e325970
lilmagnus <magnus_erlandsen@hotmail.com>
2023-05-25 14:39:50 +0200

    Wordfish, delt dokumentene så 1 variabel per dokument :)

--- analyse_med_XYZ.R
+++ analyse_med_XYZ.R
@@ -8,6 +8,8 @@ library(tidytext)
 library(quanteda.textmodels)
 library(quanteda.textplots)
 
+
+
 ### Read PDFs
 dat_raw <- readtext("Commission consultation all/*")
 
@@ -25,11 +27,24 @@ saveRDS(dat, "data_corpus_aiact.rds")
 
 ai_act <- readRDS("data_corpus_aiact.rds")
 
+
+## Del opp til 2 forskjellige variabler
+text1_act <- ai_act[1,]
+text1_act
+
+text2_act <- ai_act[2,]
+text2_act
+
+## Lag corpuser, av hele, og begge hver for seg
+
 corp_aiact <- corpus(ai_act)
 corp_aiact
 
+corp_txt1 <- corpus(text1_act)
+corp_txt2 <- corpus(text2_act)
 
-### Do some thing og thang
+
+### Tokenize dokumentet og gjør til dfm
 
 ntoken(corp_aiact, remove_punct = TRUE)
 
@@ -40,6 +55,16 @@ dfm_act <- dfm(toks_act)
 dfm_act
 
 topfeatures(dfm_act, n = 10)
+
+# Repeter på de delte dokumentene
+
+txt1_act <- tokens(corp_txt1, remove_punct = TRUE) %>%
+  tokens_remove(stopwords(language = "en"))
+dfm_txt1 <- dfm(txt1_act)
+
+txt2_act <- tokens(corp_txt2, remove_punct = TRUE) %>%
+  tokens_remove(stopwords(language = "en"))
+dfm_txt2 <- dfm(txt2_act)
 
 
 ### --------Wordcloud(?)----
@@ -99,9 +124,26 @@ corp_aiact %>%
 # Positive ord blir oftere brukt. (???)
 
 
-### Wordfish (?)
+### Wordfish
 
 tmod_wf <- textmodel_wordfish(dfm_act, dir = c(1,2))
 summary(tmod_wf)
 
 textplot_scale1d(tmod_wf, groups = dfm_act$doc_id)
+
+textplot_scale1d(tmod_wf, margin = "features",
+                 highlighted = c("ai", "risk", "criminal", "harm",
+                                 "rights", "compliance", "ensure",
+                                 "world", "appropriations", "budget",
+                                 "deleted", "year", "regulation",
+                                 "multiannual", "sensetive", "paper",
+                                 "0.760", "machine"))
+
+# Estimated theta = estimated document positions
+# Estimated beta = estimated feature marginal effects
+# Estimated psi = estimated word fixed effects
+
+
+
+### Keywords in context (?) / multiword expressions
+
